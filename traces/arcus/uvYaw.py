1e26f6f21855e99e0fbf1e7fcec1986b4182e9b8
rallured <rallured@gmail.com>
2015-03-03 17:25:43 -0500

    Worked on UV Yaw Simulation

--- traces/arcus/uvYaw.py
+++ traces/arcus/uvYaw.py
@@ -17,7 +17,7 @@ import pdb
 #Write raytrace with beam impact orientation and
 #grating orientation as free parameters
 
-def alignTrace(inc,impact,grating,order=0):
+def alignTrace(inc,impact,grating,detector,order=0):
     """Traces UV laser rays to grating. Beam impact misalignment
     is handled with a single coordinate transformations right after
     source definition. Grating orientation is handled with
@@ -45,15 +45,62 @@ def alignTrace(inc,impact,grating,order=0):
     PT.itransform(*grating)
     #Go to detector depending on order
     if order is not 0:
-        PT.transform(-400.,0,0,0,0,0)
+        PT.transform(-800.,0,0,0,0,0)
     else:
-        PT.transform(400.,0,0,0,0,0)
+        PT.transform(800.,0,0,0,0,0)
     #Trace to detector
-    PT.transform(0,0,0,0,np.pi/2,0)
+    PT.transform(0,0,0,0,-np.pi/2,0)
+    PT.transform(*detector)
     PT.flat()
     #Return ray position
     return PT.x,PT.y
 
-def dofSensitivity(inc):
-    return
+def computeYaw(inc,impact,grating,detr,detd):
+    """Traces both orders and computes yaw of grating
+    Uses alignTrace
+    Returns yaw angle assuming 
+    """
+    xr,yr = alignTrace(inc,impact,grating,detr,order=0)
+    betar = 800./np.sqrt(800.**2+xr**2+yr**2)
+    alphar = -yr/np.sqrt(800.**2+xr**2+yr**2)
+    xd,yd = alignTrace(inc,impact,grating,detd,order=1)
+    betad = -800./np.sqrt(800.**2+xd**2+yd**2)
+    alphad = -yd/np.sqrt(800.**2+xd**2+yd**2)
+    
+    return np.arctan((alphad-alphar)/(betar-betad))*180./np.pi*60.**2
+
+
+def dofSensitivity(inc,alignvector,obj='beam',dof=0):
+    """Compute x,y positions of reflected and diffracted spots
+    return as a function of alignvector
+    """
+    #Initialize misalignment vectors
+    grating = np.zeros(6)
+    impact = np.zeros(6)
+
+    #Initialize output vectors
+    xr = np.zeros(np.size(alignvector))
+    yr = np.copy(xr)
+    xd = np.copy(xr)
+    yd = np.copy(xr)
+
+    #Perform raytraces in loop
+    for a in alignvector:
+        #Adjust misalignments
+        if obj is 'beam':
+            impact[dof] = a
+        else:
+            grating[dof] = a
+
+        #Perform trace and set appropriate output elements
+        i = a==alignvector
+        x,y = alignTrace(inc,impact,grating,order=0)
+        xr[i] = x
+        yr[i] = y
+        x,y = alignTrace(inc,impact,grating,order=1)
+        xd[i] = x
+        yd[i] = y
+
+    #Return
+    return xr,yr,xd,yd
 
